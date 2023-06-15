2d4419120d2ae81969a36aae73b99e4ca30f1a5d
UtkarshCyber <83854791+UtkarshCyber@users.noreply.github.com>
2023-06-16 02:29:49 +0530

    Update local-wr.py

--- Locally-WR/local-wr.py
+++ Locally-WR/local-wr.py
@@ -1,53 +1,69 @@
-rom numpy import *
-from os import listdir
-import matplotlib
+import numpy as np # linear algebra
+import pandas as pd # data processing
 import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np1
-import numpy.linalg as np
-from scipy.stats.stats import pearsonr
+import warnings
+warnings.filterwarnings('ignore')
 
-def kernel(point,xmat, k):
- m,n = np1.shape(xmat)
- weights = np1.mat(np1.eye((m)))
- for j in range(m):
-    diff = point - X[j]
-    weights[j,j] = np1.exp(diff*diff.T/(-2.0*k**2))
- return weights
+class LocallyWeightedRegression:
+    #maths behind Linear Regression:
+    # theta = inv(X.T*W*X)*(X.T*W*Y)this will be our theta whic will 
+    # be learnt for each point
+    # initializer of LocallyWeighted Regression that stores tau as parameters
+    def __init__(self, tau = 0.01):
+        self.tau = tau
+    def kernel(self, query_point, X):
+        Weight_matrix = np.mat(np.eye(len(X)))
+        for idx in range(len(X)):
+            Weight_matrix[idx,idx] = np.exp(np.dot(X[idx]-query_point, (X[idx]-query_point).T)/(-2*self.tau*self.tau))
+        return Weight_matrix
+    # function that makes the predictions of the output of a given query point
+    def predict(self, X, Y, query_point):
+        q = np.mat([query_point, 1])
+        X = np.hstack((X, np.ones((len(X), 1))))
+        W = self.kernel(q, X)
+        theta = np.linalg.pinv(X.T*(W*X))*(X.T*(W*Y))
+        pred = np.dot(q, theta)
+        return pred
+    #function that fits and predicts the output of all query points
+    def fit_and_predict(self, X, Y):
+        Y_test, X_test = [], np.linspace(-np.max(X), np.max(X), len(X))
+        for x in X_test:
+            pred = self.predict(X, Y, x)
+            Y_test.append(pred[0][0])
+        Y_test = np.array(Y_test)
+        return Y_test
+    # function that computes the score rmse
+    def score(self, Y, Y_pred):
+        return np.sqrt(np.mean((Y-Y_pred)**2))
+    # function that fits as well as shows the scatter plot of all points
+    def fit_and_show(self, X, Y):
+        Y_test, X_test = [], np.linspace(-np.max(X), np.max(X), len(X))
+        for x in X_test:
+            pred = self.predict(X, Y, x)
+            Y_test.append(pred[0][0])
+        Y_test = np.array(Y_test)
+        plt.style.use('seaborn')
+        plt.title("The scatter plot for the value of tau = %.5f"% self.tau)
+        plt.scatter(X, Y, color = 'red')
+        plt.scatter(X_test, Y_test, color = 'green')
+        plt.show()
+        
+        
+# reading the csv files of the given dataset
+dfx = pd.read_csv('./weightedX.csv')
+dfy = pd.read_csv('./weightedY.csv')
 
-def localWeight(point,xmat,ymat,k):
- wei = kernel(point,xmat,k)
- W = (X.T*(wei*X)).I*(X.T*(wei*ymat.T))
- return W
 
-def localWeightRegression(xmat,ymat,k):
- m,n = np1.shape(xmat)
- ypred = np1.zeros(m)
- for i in range(m):
-    ypred[i] = xmat[i]*localWeight(xmat[i],xmat,ymat,k)
- return ypred
+# store the values of dataframes in numpy arrays
+X = dfx.values
+Y = dfy.values
 
-data = pd.read_csv('/content/drive/MyDrive/ML data/tips.csv')
-bill = np1.array(data.total_bill)
-tip = np1.array(data.tip)
 
-#preparing and add 1 in bill
-mbill = np1.mat(bill)
-mtip = np1.mat(tip) # mat is used to convert to n dimesiona to 2 dimensional array form
-m= np1.shape(mbill)[1]
-# print(m) 244 data is stored in m
-one = np1.mat(np1.ones(m))
-X= np1.hstack((one.T,mbill.T)) # create a stack of bill from ONE
-#print(X)
-#set k here
-ypred = localWeightRegression(X,mtip,2)
-SortIndex = X[:,1].argsort(0)
-xsort = X[SortIndex][:,0]
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.scatter(bill,tip, color='blue')
-ax.plot(xsort[:,1],ypred[SortIndex], color = 'red', linewidth=5)
-plt.xlabel('Total bill')
-plt.ylabel('Tip')
-plt.show()
+# normalising the data values
+u = X.mean()
+std = X.std()
+X = ((X-u)/std)
+tau = 0.2
+model = LocallyWeightedRegression(tau)
+Y_pred = model.fit_and_predict(X, Y)
+model.fit_and_show(X, Y)
